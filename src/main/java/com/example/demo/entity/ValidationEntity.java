e0f0d897ead1e3486b3053f3340929a35170edaa
bala-amypo <bala@amypo.in>
2025-12-18 08:22:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -13,13 +13,55 @@ public class ValidationEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    @Size(min=2, max=10, message="must be 2 to 10 character)
+    @Size(min=2, max=10, message="must be 2 to 10 character")
     private String username;
     @Email(message = "Email is not valid")
     private String email;
-    @Size(min =2 , max=6)
-    @NotNull(message="Password is mandatory");
+    @Size(min =2 , max=6, message="must be 2 to 6 character")
+    @NotNull(message="Password is mandatory")
     private String password;
     @Max(30)
     private int age;
+
+     public void setId(Long id){
+        this.id=id;
+    }
+    public void setUserName(String username){
+        this.name=name;
+    }
+    public void setUsername(String email){
+        this.username=username;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public void setCreated(Date created){
+        this.created=created;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public Date getCreated(){
+        return created;
+    }
+
+    public StudentEntity(){};
+    public StudentEntity(Integer id, String name, String username, String password, Date created){
+        this.id=id;
+        this.name=name;
+        this.username=username;
+        this.password=password;
+        this.created=created;
+    }
+ 
 }
