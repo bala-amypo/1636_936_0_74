1ee3abc160e22c072d05be47f40ac3b9bfe7b850
bala-amypo <bala@amypo.in>
2025-12-18 08:07:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,29 +1,31 @@
 package com.example.demo.controller;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Path;
 import java.util.List;
 
-
 @RestController
-public class StudentController{
-    @Autowired StudentService ser;
+public class StudentController {
+    @Autowired
+    StudentService ser;
+
     @PostMapping("/post")
-    public StudentEntity sendData(@RequestBody StudentEntity stu){
+    public StudentEntity sendData(@RequestBody StudentEntity stu) {
         return ser.postData(stu);
     }
+
     @PostMapping("/get")
-    public List<StudentEntity>getVal(){
+    public List<StudentEntity> getVal() {
         return ser.getAllData();
     }
-    @PostMapping("/delete/{id}"){
-        public String deleteValue(@PathVariable int id){
-            return ser.deleteData(id);
-        }
+
+    @PostMapping("/delete/{id}")
+    public String deleteValue(@PathVariable int id) {
+        return ser.deleteData(id);
     }
 }
