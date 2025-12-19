50cd5927e306dfd6566d74cdada94671681fc5ae
bala-amypo <bala@amypo.in>
2025-12-19 07:52:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimestampEntity.java
+++ src/main/java/com/example/demo/entity/TimestampEntity.java
@@ -22,12 +22,7 @@ public class ValidationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull
-    @Size(min = 2, max = 10, message = "must be 2 to 10 characters")
-    private String username;
-
-    @Email(message = "Email is not valid")
+    private String name;
     private String email;
-
+    
 }
