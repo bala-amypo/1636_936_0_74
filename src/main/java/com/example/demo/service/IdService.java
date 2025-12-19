27b95e71fc1125efb990442e9808ba9ec2b43871
bala-amypo <bala@amypo.in>
2025-12-19 09:53:59 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/IdService.java
@@ -0,0 +1,11 @@
+
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.entity.StuEntity;
+
+public interface  StuService{
+    iEntity postData( StuEntity stu); //method declaration
+    // List< StuEntity> getAllData();
+    // String deleteData (int id);
+    //  StuEntity getData(Long id);
+}
