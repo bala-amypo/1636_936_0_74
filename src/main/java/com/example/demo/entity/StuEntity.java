b16ffb05e9cd8d18cb24e55e8d3042883890bd1b
bala-amypo <bala@amypo.in>
2025-12-19 09:45:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/StuEntity.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimestampEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    LocalDateTime now =LocalDateTime.now();
+    @PrePersist
+    public void Oncreate(){
+        this.createAt = now;
+        this.updateAt = now;
+    }
+    @PreUpdate
+    public void Onupdate(){
+        this.updateAt = now;
+    }
+}
