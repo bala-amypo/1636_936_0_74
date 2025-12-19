c73dbc114aa6f3ae1b4d69139f69e6afea160392
bala-amypo <bala@amypo.in>
2025-12-19 09:54:06 +0000

    User commit

--- src/main/java/com/example/demo/service/IdService.java
+++ src/main/java/com/example/demo/service/IdService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.StuEntity;
 
-public interface  StuService{
+public interface Service{
     iEntity postData( StuEntity stu); //method declaration
     // List< StuEntity> getAllData();
     // String deleteData (int id);
